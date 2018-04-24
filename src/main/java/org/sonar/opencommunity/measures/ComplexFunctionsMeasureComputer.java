240292fb7a89cb2d98a7db1c0d4906c30c038a5b
Eric Lemes <ericlemes@gmail.com>
2018-04-24 16:32:08 -0700

    Implementing measure computer for Complex Functions metric.

---
+++ src/main/java/org/sonar/opencommunity/measures/ComplexFunctionsMeasureComputer.java
@@ -0,0 +1,49 @@
+/**
+ * Sonar OpenCommunity Metrics (Community)
+ * Copyright (C) 2017 SonarOpenCommunity
+ * http://github.com/SonarOpenCommunity/sonar-opencommunity-metrics
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.opencommunity.measures;
+
+import org.sonar.api.ce.measure.Component;
+import org.sonar.api.ce.measure.Measure;
+import org.sonar.api.ce.measure.MeasureComputer;
+import org.sonar.opencommunity.metrics.OpenCommunityMetrics;
+
+public class ComplexFunctionsMeasureComputer implements MeasureComputer {
+
+  @Override
+  public MeasureComputerDefinition define(MeasureComputerDefinitionContext context) {
+    return context.newDefinitionBuilder()
+      .setOutputMetrics(OpenCommunityMetrics.COMPLEX_FUNCTIONS.key())
+      .build();       
+  }
+
+  private int sum = 0;
+
+  @Override
+  public void compute(MeasureComputerContext context) {               
+    if (context.getComponent().getType() == Component.Type.PROJECT)
+      context.addMeasure(OpenCommunityMetrics.COMPLEX_FUNCTIONS.key(), sum);
+    else if (context.getComponent().getType() == Component.Type.FILE){
+      Measure m = context.getMeasure(OpenCommunityMetrics.COMPLEX_FUNCTIONS.key());
+      if (m != null)
+        sum += m.getIntValue();
+    }    
+  }
+  
+}
---
+++ src/test/java/org/sonar/opencommunity/measures/TestComplexFunctionsMeasureComputer.java
@@ -0,0 +1,93 @@
+/**
+ * Sonar OpenCommunity Metrics (Community)
+ * Copyright (C) 2017 SonarOpenCommunity
+ * http://github.com/SonarOpenCommunity/sonar-opencommunity-metrics
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.opencommunity.measures;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import org.mockito.Mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.MockitoAnnotations;
+import org.sonar.api.ce.measure.Component;
+import org.sonar.api.ce.measure.MeasureComputer;
+import org.sonar.opencommunity.metrics.OpenCommunityMetrics;
+
+public class TestComplexFunctionsMeasureComputer {
+  private ComplexFunctionsMeasureComputer measureComputer;
+  
+  @Mock
+  private MeasureComputer.MeasureComputerDefinitionContext definitionContext;
+  
+  @Mock
+  private MeasureComputer.MeasureComputerDefinition.Builder definitionContextBuilder;
+  
+  @Mock
+  private MeasureComputer.MeasureComputerContext context;
+  
+  @Before
+  public void setUp(){
+    MockitoAnnotations.initMocks(this);
+    this.measureComputer = new ComplexFunctionsMeasureComputer();
+    
+    when(this.definitionContext.newDefinitionBuilder()).thenReturn(definitionContextBuilder);
+    when(this.definitionContextBuilder.setOutputMetrics(anyString())).thenReturn(this.definitionContextBuilder);
+  }
+  
+  @Test
+  public void testWhenDefiningShouldReturnCorrectDefinition(){
+    this.measureComputer.define(definitionContext);
+    verify(this.definitionContextBuilder).setOutputMetrics(OpenCommunityMetrics.COMPLEX_FUNCTIONS.key());
+    verify(this.definitionContextBuilder).build();
+  }
+  
+  @Test
+  public void testWhenCallingComputeShouldComputeSumCorrectly(){        
+    MeasureComputerTestHelper.setupComponentAndIntMeasure(context, Component.Type.FILE, 3, OpenCommunityMetrics.COMPLEX_FUNCTIONS.key());    
+    this.measureComputer.compute(context);
+    
+    MeasureComputerTestHelper.setupComponentAndIntMeasure(context, Component.Type.FILE, 2, OpenCommunityMetrics.COMPLEX_FUNCTIONS.key());    
+    this.measureComputer.compute(context);
+    
+    MeasureComputerTestHelper.setupProject(context);
+    this.measureComputer.compute(context);
+    
+    verify(context).addMeasure(OpenCommunityMetrics.COMPLEX_FUNCTIONS.key(), 5);
+  }
+  
+  @Test
+  public void testWhenCallingComputeWithUnexpectedComponentShouldNotAddMeasure(){
+    MeasureComputerTestHelper.setupView(context);
+    
+    this.measureComputer.compute(context);
+    verify(context, times(0)).addMeasure(anyString(), anyInt());
+  }  
+  
+  @Test
+  public void testWhenCallingComputeWithFileWithNoMeasureShouldNotThrow(){
+    MeasureComputerTestHelper.setupComponentAndIntMeasure(context, Component.Type.FILE, 42, OpenCommunityMetrics.COMPLEX_FUNCTIONS.key());    
+    when(this.context.getMeasure(OpenCommunityMetrics.COMPLEX_FUNCTIONS.key())).thenReturn(null);
+    this.measureComputer.compute(context);
+    
+    verify(context, times(0)).addMeasure(anyString(), anyInt());
+  }
+}
